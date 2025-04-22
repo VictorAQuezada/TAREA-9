b2d3092eefbf78f86625a8bb07c82f28ff5f7c1e
VictorAQuezada <victorquezada220404@gmail.com>
2025-04-22 18:29:40 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace MisLenguajesFavoritos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ir_html_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/HTML", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void Ir_css_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Hoja_de_estilos_en_cascada", BrowserLaunchMode.SystemPreferred);
+        }
+
+        // Agrega más métodos para los otros botones siguiendo el mismo patrón
+    }
+}
